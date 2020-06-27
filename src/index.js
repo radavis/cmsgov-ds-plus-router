837a6a566ef30943d31ef988675919b49f862692
Richard Davis <rdavis.bacs@gmail.com>
2020-06-27 07:52:59 -0400

    add @cmsgov/design-system

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './index.scss';
+import Sidebar from './Sidebar';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Sidebar />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/navItems.js
@@ -0,0 +1,57 @@
+const navItems = [
+  {
+    label: "Key State Personnel",
+    id: "key-state-personnel-nav",
+    items: [
+      {
+        label: "State Director and Office Address",
+        id: "state-director-and-office-address-nav",
+        url: "/key-state-personnel#state-director-and-office-address"
+      },
+      {
+        label: "Key Personnel and Program Management",
+        id: "key-personnel-and-program-management-nav",
+        url: "/key-state-personnel#key-personnel-and-program-management"
+      }
+    ]
+  },
+  {
+    label: "Program Summary",
+    id: "program-summary-nav",
+    url: "/program-summary"
+  },
+  {
+    label: "Results of Previous Activities",
+    id: "results-of-previous-activities-nav",
+    items: [
+      {
+        label: "Prior Activities Overview",
+        id: "prior-activities-overview-nav",
+        url: "/results-of-previous-activities#prior-activities-overview"
+      },
+      {
+        label: "Actual Expenditures",
+        id: "actual-expenditures-nav",
+        url: "/results-of-previous-activities#actual-expenditures"
+      }
+    ]
+  },
+  {
+    label: "Program Activities",
+    id: "program-activities-nav",
+    items: [
+      {
+        label: "Activity List",
+        id: "activity-list-nav",
+        url: "/program-activites"
+      },
+      {
+        label: "Activity 1",
+        id: "activity-1-nav",
+        url: "/program-activites/1"
+      }
+    ]
+  }
+];
+
+export default navItems;
